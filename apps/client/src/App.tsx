a28338979ccd677a9876d9bd9ae1fabf2f801ae9
LeeviHalme <29179759+LeeviHalme@users.noreply.github.com>
2024-01-19 01:37:36 +0200

    lint: add new rule and run 'yarn lint'

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -2,14 +2,14 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 
-function App() {
+const App = () => {
   const [count, setCount] = useState(0);
 
   return (
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src="/assets/vite.svg" className="logo" alt="Vite logo" />
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank" rel="noreferrer">
           <img src={reactLogo} className="logo react" alt="React logo" />
@@ -27,6 +27,6 @@ function App() {
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
     </>
   );
-}
+};
 
 export default App;
